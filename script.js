98b3c485574b3bee5a1f03328eb406241fc61107
Ross22M <144200562+Ross22M@users.noreply.github.com>
2023-12-03 07:34:30 -0300

    Ante proyecto Snape

--- script.js
+++ script.js
@@ -1,91 +1,121 @@
+   document.addEventListener('DOMContentLoaded', () => {
+    const board = document.getElementById('board');
+    const cells = document.querySelectorAll('.cell');
+    const restartButton = document.getElementById('restart-button');
+    const resetScoreButton = document.getElementById('reset-score-button');
+    const turnDisplay = document.getElementById('turn-display');
+    const scoreDisplay = document.getElementById('score-display');
 
-let intentos = 6;
-let diccionario = ['PERLA', 'PERAS', 'CARRO', 'LUJOS', 'LOBOS', 'LUCIA', 'NICOL', 'AVION', 'VOLAR', 'JUEGO']
+    let currentPlayer = 'X';
+    let gameBoard = ['', '', '', '', '', '', '', '', ''];
+    let gameActive = true;
+    let scoreX = 0;
+    let scoreO = 0;
 
-//La libreria Math sirve para contar cuantos elementos son los que estan 
-//dentro del arreglo y comenzar a autoseleccionar cual de estas es la palabra oculta
-let palabra = diccionario[Math.floor(Math.random() * diccionario.length)];
-console.log(palabra);
-
-//Aca utilizo una API para aleatorizar las palabras para el wordle (Las palabras estan en ingles)
-// fetch('https://random-word-api.herokuapp.com/word?length=5&lang=en')
-//  	.then(response => response.json())
-//  	.then(response => {
-//          console.log(response)
-//          palabra = response[0].toUpperCase()
-//      })
-//  	.catch(err => console.error(err));
-
-// window.addEventListener('load', init)
-// function init(){
-//     console.log('Esto se ejecuta solo cuando se carga la pagina web')
-// }
-
-//La funcion intentar aparece en el tutorial sin embargo
-//le hice una serie de modificaciones para que funcione correctamente 
-function intentar(){
-    const INTENTO = leerIntento();
-    if (INTENTO === palabra ) {
-        terminar("<h1>GANASTE</h1>");
-        //La funcion mostrarPalabraCorrecta(), esta para mostrar la palabra que escribiste y te dio la victoria
-        mostrarPalabraCorrecta();
-        return
+    function updateTurnDisplay() {
+        turnDisplay.textContent = `Turno de: ${currentPlayer}`;
     }
-    const GRID = document.getElementById("grid");
-    const ROW = document.createElement('div');
-    ROW.className = 'row';
-    for (let i in palabra){
-        const SPAN = document.createElement('span');
-        SPAN.className = 'letter';
-        if (INTENTO[i]===palabra[i]){
-            SPAN.innerHTML = INTENTO[i];
-            SPAN.style.backgroundColor = 'green'
-        } else if( palabra.includes(INTENTO[i]) ) {
-            SPAN.innerHTML = INTENTO[i];
-            SPAN.style.backgroundColor = 'yellow';
-        } else {
-            SPAN.innerHTML = INTENTO[i];
-            SPAN.style.backgroundColor = 'red';
+
+    function updateScore() {
+        scoreDisplay.textContent = `Puntuación - Jugador X: ${scoreX} | Jugador O: ${scoreO}`;
+    }
+
+    function resetCellStyles() {
+        cells.forEach(cell => {
+            cell.classList.remove('winner-cell');
+        });
+    }
+
+    function checkWinner() {
+        const winningCombinations = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // Filas
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columnas
+            [0, 4, 8], [2, 4, 6]             // Diagonales
+        ];
+
+        for (const combination of winningCombinations) {
+            const [a, b, c] = combination;
+            if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+                highlightWinner([a, b, c]);
+                return gameBoard[a];
+            }
         }
-        ROW.appendChild(SPAN)
+
+        return null;
+     }
+
+     function highlightWinner(winningCells) {
+        winningCells.forEach(index => {
+            cells[index].classList.add('winner-cell');
+        });
+     }
+
+     function checkDraw() {
+        return gameBoard.every(cell => cell !== '');
+     }
+
+     function handleCellClick(index) {
+        if (!gameActive || gameBoard[index] !== '') {
+            return;
+        }
+
+        resetCellStyles(); // Limpiar estilos antes de cada jugada
+
+     // Decidir aleatoriamente si el jugador actual será 'X' o 'O'
+     //currentPlayer = Math.random() < 0.5 ? 'X' : 'O'//;
+        
+        // Verificar si el jugador actual puede hacer un movimiento
+
+        if (gameBoard.filter(cell => cell !== '').length % 2 === 0 && currentPlayer === 'O') {
+           return;
+        }
+        gameBoard[index] = currentPlayer;
+        cells[index].textContent = currentPlayer;
+
+        const winner = checkWinner();
+        if (winner) {
+            alert(`Player ${winner} wins!`);
+            gameActive = false;
+            if (winner === 'X') {
+                scoreX++;
+            } else {
+                scoreO++;
+            }
+            updateScore();
+        } else if (checkDraw()) {
+            alert('It\'s a draw!');
+            gameActive = false;
+        } else {
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        }
+
+        updateTurnDisplay();
     }
-    GRID.appendChild(ROW)
-		intentos--
-    if (intentos==0){
-        terminar("<h1>PERDISTE</h1>")
-        button.style.display = "none";
-        //La funcion mostrarPalabraCorrecta(), esta para mostrar la palabra que debiste haber escrito
-        mostrarPalabraCorrecta();
+
+    function restartGame() {
+        gameBoard = ['', '', '', '', '', '', '', '', ''];
+        cells.forEach(cell => {
+            cell.textContent = '';
+            cell.classList.remove('winner-cell');
+        });
+        gameActive = true;
+        currentPlayer = 'X';
+        updateTurnDisplay();
     }
-}
 
-//Esta funcion fue agregada para digitar en la pantalla la frase 'la palabra correcta era [palabra]'
-function mostrarPalabraCorrecta() {
-    const resultadoDiv = document.getElementById("resultado");
-    resultadoDiv.innerHTML = `<p>La palabra correcta era: ${palabra}</p>`;
-}
+    function resetScore() {
+        scoreX = 0;
+        scoreO = 0;
+        updateScore();
+    }
 
+    cells.forEach((cell, index) => {
+        cell.addEventListener('click', () => handleCellClick(index));
+    });
 
-const button = document.getElementById("guess-button");
-button.addEventListener("click", intentar);
-const input = document.getElementById("guess-input");
-const valor = input.value;
-
-function leerIntento(){
-    let intento = document.getElementById("guess-input");
-    intento = intento.value;
-    intento = intento.toUpperCase(); 
-    return intento;
-}
-
-function terminar(mensaje){
-    const INPUT = document.getElementById("guess-input");
-    INPUT.disabled = true;
-    button.disabled = true;
-    let contenedor = document.getElementById('guesses');
-    contenedor.innerHTML = mensaje;
-}
-
-//Esta linea de codigo fue implementada para el arreglo que contiene mas de un dato tipo texto
-Math.floor(Math.random() * 10) + 1;
-
+    restartButton.addEventListener('click', restartGame);
+    resetScoreButton.addEventListener('click', resetScore);
+    
+    updateTurnDisplay();
+    updateScore();
+})
